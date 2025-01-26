031b8dc9e0655fe3c3c2f8c47b49569f75414d61
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-26 00:20:29 -0300

    refactor: remove server listening logic from app.js and export app for testing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { generateItineraryHandler } from "./controllers/itineraryController.js";
-import { PORT } from "./config/dotenv.js";
 
 const app = express();
 app.use(cors());
@@ -10,6 +9,4 @@ app.use(bodyParser.json());
 
 app.post("/generate-itinerary", generateItineraryHandler);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
