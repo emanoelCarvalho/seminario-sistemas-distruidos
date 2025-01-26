add35eb4d62f67d46c1b1af381e4c15271bea483
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-26 00:27:38 -0300

    :recycle: refactor: move server listening logic from server.js to app.js and update start script in package.json

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { generateItineraryHandler } from "./controllers/itineraryController.js";
+import { PORT } from "./config/dotenv.js";
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/generate-itinerary", generateItineraryHandler);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
