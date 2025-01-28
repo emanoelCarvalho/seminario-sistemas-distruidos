6dd18687ac49987e0949b1f516899e35c6a02b9d
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-27 22:17:10 -0300

    chore: simplify start.sh script by removing unnecessary comments and ensuring proper formatting

--- start.sh
+++ start.sh
@@ -2,16 +2,14 @@
 
 # Iniciar o servidor backend (Express)
 echo "Starting the server backend..."
-cd /home/ehcs/Documentos/seminario-sistemas-distruidos/api/src/  # Substitua pelo diretório correto onde está o package.json do seu backend
-npm install  # (opcional) se necessário instalar as dependências
-npm start &  # Executa o servidor Express em segundo plano
+cd /home/ehcs/Documentos/seminario-sistemas-distruidos/api/src/  
+npm install  
+npm start &  
 
 # Iniciar o servidor frontend (usando PHP)
 echo "Starting the client frontend..."
-cd /home/ehcs/Documentos/seminario-sistemas-distruidos/client/  # Substitua pelo diretório onde está o index.html
-php -S localhost:8000 &  # Inicia um servidor PHP simples para servir o frontend
-
+cd /home/ehcs/Documentos/seminario-sistemas-distruidos/client/  
+php -S localhost:8000 & 
 echo "Server started!"
 
-# Manter o script ativo
-wait  # Isso mantém o script ativo até que os processos sejam encerrados
+wait 
